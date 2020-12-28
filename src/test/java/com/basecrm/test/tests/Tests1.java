d64367ea425c1872767602bd1e15969ae9b94390
awettedla <awettedla@gmail.com>
2020-12-28 16:13:58 -0500

    Initial commit

---
+++ src/test/java/com/basecrm/test/tests/Tests1.java
@@ -0,0 +1,16 @@
+package com.basecrm.test.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Tests1 {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://www.google.com");
+    }
+}
